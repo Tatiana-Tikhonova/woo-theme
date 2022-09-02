7637b1183e687d58d79cbbb6e52b2294cafdd2ef
Tatyana-Tikhonova <tantikh2020@gmail.com>
2022-09-02 09:46:35 +0300

    edit woocommerce.php

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -22,12 +22,12 @@ function tati_woocommerce_setup()
 	add_theme_support(
 		'woocommerce',
 		array(
-			'thumbnail_image_width' => 150,
-			'single_image_width'    => 300,
+			'thumbnail_image_width' => 300,
+			'single_image_width'    => 600,
 			'product_grid'          => array(
 				'default_rows'    => 3,
 				'min_rows'        => 1,
-				'default_columns' => 4,
+				'default_columns' => 3,
 				'min_columns'     => 1,
 				'max_columns'     => 6,
 			),
